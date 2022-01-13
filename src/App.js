037a4ec3ee9b65a3b267b29756b025dd0d6e9e27
satyam kushwaha <satyam21@navgurukul.org>
2022-01-13 22:16:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [todoList, setDotoList] = useState([])
+
+  const onChangeValue = (event) => {
+    setInputValue(event.target.value)
+  }
+  const submit = () => {
+    setDotoList([...todoList, inputValue])
+    setInputValue("")
+  }
+  const deleteBtn = (e, index1) => {
+    setDotoList(todoList.filter((item, index) => index !== index1))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app ">
+      <div className="main">
+        <h1 >To-Do List </h1>
+        <div className='addTask'>
+          <input type="text"
+            className="form-control"
+            placeholder="Enter to do work"
+            value={inputValue}
+            onChange={onChangeValue}
+          />
+
+          <button onClick={() => submit()} className="btn btn-primary">Add</button>
+
+        </div>
+
+      </div>
+
+      <div className='list'>
+        {todoList && todoList.length > 0 && todoList.map((item, index) => {
+          return (
+
+            <h4 key={index} >
+              <span>
+                {index + 1}.{"  "+item}
+              </span>
+              <button onClick={(e) => deleteBtn(e, index)} className='btn btn-danger'>X</button>
+            </h4>
+          )
+        })}
+      </div>
     </div>
+
   );
 }
 
